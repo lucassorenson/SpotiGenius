d73bb41d17e9007cd7844cafafd8733a6de54ba1
Lucas Sorenson <lucasrsorenson@gmail.com>
2020-01-24 10:25:04 -0500

    basic express + node setup

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+const folderForIndex = app.get("env") === 'development' ? 'public' : 'build'
 
+app.use(cors({ origin: 'http://localhost:3000' }))
+
+app.get('/', (req, res) => {
+    res.send('root route')
+})
+
+app.use(express.static(path.join(__dirname, `client/${folderForIndex}`)));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + `/client/${folderForIndex}/index.html`));
+})
+
+app.listen(port, (req, res) => {
+    console.log(`server listening on port: ${port}`)
+});
